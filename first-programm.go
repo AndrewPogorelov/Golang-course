c204c12266f5e288d5b1898ff490609258e99849
AndreyPogorelov <pogorelov1441@gmail.com>
2022-09-27 14:36:11 +0300

    update2

--- first-programm.go
+++ first-programm.go
@@ -3,5 +3,5 @@ package main
 import "fmt" // импортировали пакет ввода/вывода.
 
 func main() {
-	fmt.Println("Hello, Go!") // используем встроенную функцию Println() из пакета fmt.
+	fmt.Println(string("Hello, Go!"[4])) // используем встроенную функцию Println() из пакета fmt.
 }
