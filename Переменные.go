82cac2eec8e87de541b60340f3232930f60234ba
AndreyPogorelov <pogorelov1441@gmail.com>
2022-09-27 14:57:44 +0300

    update3

--- Переменные.go
+++ Переменные.go
@@ -12,4 +12,18 @@ func main() {
 
 	fmt.Println(Hello)
 	fmt.Println(a)
+
+	// Можно группировать:
+	var (
+		x int     = 10
+		c string  = "Что бы спросить?"
+		z float64 = 1.045
+	)
+	fmt.Println(x, c, z)
+
+	// А еще можно записать так:
+	var symbol int32 = '\u7777'
+	var symbol1 uint64 = '\u5555' // будет какая нибудь фигня в виде иероглифа
+	fmt.Println(string(symbol) + "" + string(symbol1))
+
 }
